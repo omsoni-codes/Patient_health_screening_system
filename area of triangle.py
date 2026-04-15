8a9b1199763eaa1fef6c1ee2cad1d03021da6a6b
om_soni <omsoni0507soni@gmail.com>
2026-04-15 19:40:19 +0530

    Revert "new"
    This reverts commit 38c66e471630a1fe0e0c959e8ca3116da3b18e95.

---
+++ area of triangle.py
@@ -0,0 +1,5 @@
+base = float (input("value of b "))
+hight = float (input("value of hight "))
+area_of_triangle = 0.5 * (base * hight)
+print ("area of triangle is ",(float(area_of_triangle)  ),"cm2", sep="")
+
