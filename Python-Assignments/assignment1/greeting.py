bc176f86e4db663121f70a52986fa4b643fe2d27
Om Soni <omsoni0507soni@gmail.com>
2026-04-15 20:04:10 +0530

    Delete Python-Assignments directory

--- Python-Assignments/assignment1/greeting.py
+++
@@ -1,3 +0,0 @@
-f1 =input("Enter your first name: ")
-f2 =input("Enter your last name: ")
-print(f"Hello {f1} {f2}! Welcome to Python program")
--- Python-Assignments/assignment2/sum_1_to_50.py
+++
@@ -1,5 +0,0 @@
-total = 0
-for i in range(1, 51):
-    total += i
-print("The sum of integers from 1 to 50 is:", total)
-input("Press Enter to exit...")
