ebabe436e940b92b9ba975b8955291ad1ab76e59
om_soni <omsoni0507soni@gmail.com>
2026-04-15 19:43:31 +0530

    new

--- area of triangle.py
+++
@@ -1,5 +0,0 @@
-base = float (input("value of b "))
-hight = float (input("value of hight "))
-area_of_triangle = 0.5 * (base * hight)
-print ("area of triangle is ",(float(area_of_triangle)  ),"cm2", sep="")
-
